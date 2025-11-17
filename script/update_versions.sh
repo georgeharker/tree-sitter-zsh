b3b97b98a7da3291d5310ddbff18290d458ed561
George Harker <george@george-graphics.co.uk>
2025-11-17 12:45:22 -0800

    use tree-sitter version script

--- script/update_versions.sh
+++ script/update_versions.sh
@@ -7,9 +7,7 @@ else
 fi
 
 version=$1
-sed "${inplace[@]}" -Ee 's/version *= *"([0-9]+\.[0-9]+\.[0-9]+)"/version = "'${version}'"/' Cargo.toml
-sed "${inplace[@]}" -Ee 's/"version": *"([0-9]+\.[0-9]+\.[0-9]+)"/"version": "'${version}'"/' package.json
-sed "${inplace[@]}" -Ee 's/version *= *"([0-9]+\.[0-9]+\.[0-9]+)"/version = "'${version}'"/' pyproject.toml
+tree-sitter version ${version}
 
 npm install --package-lock-only
 cargo update
